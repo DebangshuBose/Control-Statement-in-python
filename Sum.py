4acb9c514f2295d29b2e7b994f0533e9fab8d531
DebangshuBose <sahilbasu63@gmail.com>
2025-07-13 08:37:57 +0530

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,9 @@
+# Initialize the sum
+i= 0
+
+# Loop through numbers from 1 to 50
+for number in range(1, 51):
+    i =i+ number  # Add each number to the total
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", i)
---
+++ newfile.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the no"))
+if a%2==0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
